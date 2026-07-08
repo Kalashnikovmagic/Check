2079b2cafdd58024221ab4b205ec226afdc8bd55
Kalashnikovmagic <akalashnikov26@gmail.com>
2026-07-08 20:13:39 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,78 @@
-const CACHE_NAME = "magic-checklist-v1";
-
+const CACHE_NAME = "magic-checklist-v2";
 
 const FILES_TO_CACHE = [
-
     "./",
     "./index.html",
     "./style.css",
     "./app.js",
     "./manifest.json"
-
 ];
 
 
+self.addEventListener("install", event => {
 
-// Установка
+    self.skipWaiting();
 
-self.addEventListener(
-    "install",
-    event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
 
-        event.waitUntil(
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys()
+        .then(keys => {
+
+            return Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+
+                })
+
+            );
+
+        })
+        .then(() => self.clients.claim())
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        fetch(event.request)
+        .then(response => {
+
+            const clone = response.clone();
 
             caches.open(CACHE_NAME)
             .then(cache => {
+                cache.put(event.request, clone);
+            });
 
-                return cache.addAll(FILES_TO_CACHE);
+            return response;
 
-            })
+        })
+        .catch(() => {
 
-        );
+            return caches.match(event.request);
 
-    }
+        })
 
-);
+    );
 
-
-
-// Загрузка файлов
-
-self.addEventListener(
-    "fetch",
-    event => {
-
-        event.respondWith(
-
-            caches.match(event.request)
-            .then(response => {
-
-                return response || fetch(event.request);
-
-            })
-
-        );
-
-    }
-
-);
+});
