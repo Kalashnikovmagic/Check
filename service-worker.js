00d83b4f45d53f8d042f865735ac9b4ddbb92cfd
Kalashnikovmagic <akalashnikov26@gmail.com>
2026-09-24 10:27:43 +0300

    Improve Magic Checklist offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,78 +1,54 @@
-const CACHE_NAME = "magic-checklist-v2";
+const CACHE_NAME = "magic-checklist-v3";
 
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json"
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-
 self.addEventListener("install", event => {
-
-    self.skipWaiting();
-
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
 });
-
 
 self.addEventListener("activate", event => {
-
-    event.waitUntil(
-
-        caches.keys()
-        .then(keys => {
-
-            return Promise.all(
-
-                keys.map(key => {
-
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-
-                })
-
-            );
-
-        })
-        .then(() => self.clients.claim())
-
-    );
-
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
-
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
 
-    event.respondWith(
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
 
-        fetch(event.request)
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request)
         .then(response => {
+          if (!response.ok) return response;
 
-            const clone = response.clone();
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
 
-            caches.open(CACHE_NAME)
-            .then(cache => {
-                cache.put(event.request, clone);
-            });
-
-            return response;
-
+          return response;
         })
-        .catch(() => {
-
-            return caches.match(event.request);
-
-        })
-
-    );
-
+        .catch(() => caches.match("./index.html"));
+    })
+  );
 });
